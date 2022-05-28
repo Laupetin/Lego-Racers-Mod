920aeb58d92d07fbe7420851c882ffa59956de46
Jan <jan@laupetin.net>
2022-05-28 20:54:33 +0200

    add utils for magic16 and alignment

--- src/Utils/Utils/FileUtils.h
+++ src/Utils/Utils/FileUtils.h
@@ -3,6 +3,14 @@
 
 namespace utils
 {
+    static constexpr uint16_t MakeMagic16(const char ch0, const char ch1)
+    {
+        return static_cast<uint16_t>(
+            static_cast<uint16_t>(ch0)
+            | static_cast<uint16_t>(ch1) << 8
+        );
+    }
+
     static constexpr uint32_t MakeMagic32(const char ch0, const char ch1, const char ch2, const char ch3)
     {
         return static_cast<uint32_t>(ch0)
