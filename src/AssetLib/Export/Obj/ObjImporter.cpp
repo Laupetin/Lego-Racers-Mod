d59864cb01f2f7e07bf05f13d494e88f0845f597
Jan <jan@laupetin.net>
2022-11-19 10:25:25 +0100

    Use ObjReader for reading obj files when importing them in converter

--- src/AssetLib/Export/Obj/ObjImporter.cpp
+++ src/AssetLib/Export/Obj/ObjImporter.cpp
@@ -5,6 +5,7 @@
 #include <filesystem>
 
 #include "Obj.h"
+#include "ObjReader.h"
 #include "Asset/Gdb/Gdb.h"
 
 using namespace obj;
@@ -13,11 +14,6 @@ namespace fs = std::filesystem;
 
 namespace obj
 {
-    bool ReadObjModelFromFile(std::istream& stream, ObjModel& model)
-    {
-        return false;
-    }
-
     std::unique_ptr<gdb::Model> CreateGdbFromObj(const ObjModel& obj)
     {
         return nullptr;
@@ -39,22 +35,26 @@ bool ObjImporter::Convert(const std::string& directory, const std::string& fileP
     const fs::path fsPath = fs::path(directory) / filePath;
     if (!fs::is_regular_file(fsPath))
     {
-        std::cerr << "Not a valid file: \"" << fsPath << "\"\n";
+        std::cerr << "Not a valid file: \"" << fsPath.string() << "\"\n";
         return false;
     }
 
     std::ifstream in(fsPath, std::ios::in | std::ios::binary);
     if (!in.is_open())
     {
-        std::cerr << "Failed to open obj: \"" << fsPath << "\"\n";
+        std::cerr << "Failed to open obj: \"" << fsPath.string() << "\"\n";
         return false;
     }
 
-    ObjModel model;
-    if (!ReadObjModelFromFile(in, model))
+    const auto reader = ObjReader::Create(in);
+    const auto model = reader->ReadModel();
+    if (!model)
+    {
+        std::cerr << "Error while reading obj: \"" << fsPath.string() << "\"\n";
         return false;
+    }
 
-    const auto gdb = CreateGdbFromObj(model);
+    const auto gdb = CreateGdbFromObj(*model);
     if (!gdb)
         return false;
 
