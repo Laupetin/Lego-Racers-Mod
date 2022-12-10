33c6faf487db8930e4ba5219bc70981d0d092fbd
Jan <jan@laupetin.net>
2022-12-10 01:52:19 +0100

    Add unit tests for TokenOutputStream

--- test/AssetLibTests/TokenStreamTests.cpp
+++ test/AssetLibTests/TokenStreamTests.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <limits>
 #include <sstream>
+#include <type_traits>
 
 #include "TokenStream.h"
 
@@ -19,7 +20,6 @@ namespace test::token_stream
         std::stringstream ss;
         const auto stream = ITokenInputStream::Create(ss);
         ss << '\x60';
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>('\x60'));
@@ -33,7 +33,6 @@ namespace test::token_stream
         std::stringstream ss;
         const auto stream = ITokenInputStream::Create(ss);
         ss << '\x60';
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->PeekValue();
         REQUIRE(token.m_type == static_cast<token_type_t>('\x60'));
@@ -53,7 +52,6 @@ namespace test::token_stream
         std::stringstream ss;
         const auto stream = ITokenInputStream::Create(ss);
         ss << static_cast<char>(TOKEN_STRING) << "Hello World" << '\x00';
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_STRING));
@@ -71,8 +69,6 @@ namespace test::token_stream
         constexpr float f = 3.14f;
         ss.write(reinterpret_cast<const char*>(&f), sizeof(f));
 
-        ss.seekg(0, std::ios::beg);
-
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_FLOAT));
         REQUIRE_THAT(token.m_float_value, WithinAbs(static_cast<double>(3.14f), FLOAT_EPS));
@@ -88,8 +84,6 @@ namespace test::token_stream
         ss << static_cast<char>(TOKEN_INT);
         constexpr int32_t i = 1337;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_INT));
@@ -107,8 +101,6 @@ namespace test::token_stream
         constexpr int8_t i = 42;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
 
-        ss.seekg(0, std::ios::beg);
-
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_INT));
         REQUIRE(token.m_int_value == 42);
@@ -124,8 +116,6 @@ namespace test::token_stream
         ss << static_cast<char>(TOKEN_UINT8);
         constexpr uint8_t i = 142u;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_INT));
@@ -143,8 +133,6 @@ namespace test::token_stream
         constexpr int16_t i = 21588;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
 
-        ss.seekg(0, std::ios::beg);
-
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_INT));
         REQUIRE(token.m_int_value == 21588);
@@ -160,8 +148,6 @@ namespace test::token_stream
         ss << static_cast<char>(TOKEN_UINT16);
         constexpr uint16_t i = 54356u;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_INT));
@@ -179,8 +165,6 @@ namespace test::token_stream
         constexpr uint16_t i = 0x435C;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
 
-        ss.seekg(0, std::ios::beg);
-
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_FLOAT));
         REQUIRE_THAT(token.m_float_value, WithinAbs(4.21, 0.000244140625));
@@ -196,8 +180,6 @@ namespace test::token_stream
         ss << static_cast<char>(TOKEN_FP16_MAN_9);
         constexpr uint16_t i = 0x86;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_FLOAT));
@@ -215,8 +197,6 @@ namespace test::token_stream
         constexpr uint16_t i = 1337;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
 
-        ss.seekg(0, std::ios::beg);
-
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_FLOAT));
         REQUIRE_THAT(token.m_float_value, WithinAbs(1337, FLOAT_EPS));
@@ -232,8 +212,6 @@ namespace test::token_stream
         ss << static_cast<char>(TOKEN_FP8);
         constexpr int8_t i = 53;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_FLOAT));
@@ -251,8 +229,6 @@ namespace test::token_stream
         constexpr uint16_t i = 0x8446;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
 
-        ss.seekg(0, std::ios::beg);
-
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(0x8446));
 
@@ -267,8 +243,6 @@ namespace test::token_stream
 
         const auto tokenType = GENERATE(TOKEN_COMMA, TOKEN_LEFT_CURLY, TOKEN_RIGHT_CURLY, TOKEN_LEFT_BRACKET, TOKEN_RIGHT_BRACKET, TOKEN_COMMA, TOKEN_SEMICOLON, TOKEN_CUSTOM_FIRST, 99);
         ss << static_cast<char>(tokenType);
-
-        ss.seekg(0, std::ios::beg);
 
         INFO("Simple token recognized: " << tokenType);
 
@@ -290,8 +264,6 @@ namespace test::token_stream
         ss.write(reinterpret_cast<const char*>(&arraySize), sizeof(arraySize));
 
         ss << static_cast<char>(TOKEN_COMMA);
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_COMMA));
@@ -320,8 +292,6 @@ namespace test::token_stream
 
         constexpr uint16_t i = 0x8446;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(0x8446));
@@ -360,8 +330,6 @@ namespace test::token_stream
         constexpr int32_t i2 = 13361338;
         ss.write(reinterpret_cast<const char*>(&i2), sizeof(i2));
 
-        ss.seekg(0, std::ios::beg);
-
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_INT));
         REQUIRE(token.m_int_value == 1337);
@@ -387,8 +355,6 @@ namespace test::token_stream
 
         constexpr int32_t i0 = 1337;
         ss.write(reinterpret_cast<const char*>(&i0), sizeof(i0));
-
-        ss.seekg(0, std::ios::beg);
 
         auto token = stream->NextValue();
         REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_LEFT_BRACKET));
@@ -436,8 +402,6 @@ namespace test::token_stream
             constexpr int8_t value1 = 42;
             ss.write(reinterpret_cast<const char*>(&value1), sizeof(value1));
 
-            ss.seekg(0, std::ios::beg);
-
             auto token = stream->NextValue();
             REQUIRE(token.m_type == static_cast<token_type_t>(TOKEN_COMMA));
 
@@ -473,8 +437,6 @@ namespace test::token_stream
 
             constexpr int8_t value3 = -3;
             ss.write(reinterpret_cast<const char*>(&value3), sizeof(value3));
-
-            ss.seekg(0, std::ios::beg);
 
             // First struct
 
@@ -566,8 +528,6 @@ namespace test::token_stream
         constexpr int32_t i = 1337;
         ss.write(reinterpret_cast<const char*>(&i), sizeof(i));
 
-        ss.seekg(0, std::ios::beg);
-
         const auto value = stream->NextIntegerValue();
         REQUIRE(value == 1337);
 
@@ -581,8 +541,6 @@ namespace test::token_stream
         const auto stream = ITokenInputStream::Create(ss);
         ss << static_cast<char>(TOKEN_SEMICOLON);
 
-        ss.seekg(0, std::ios::beg);
-
         REQUIRE_THROWS_AS(stream->NextIntegerValue(), TokenStreamException);
     }
 
@@ -591,7 +549,6 @@ namespace test::token_stream
         std::stringstream ss;
         const auto stream = ITokenInputStream::Create(ss);
         ss << static_cast<char>(TOKEN_STRING) << "Hello World" << '\x00';
-        ss.seekg(0, std::ios::beg);
 
         const auto value = stream->NextStringValue();
         REQUIRE(value == "Hello World");
@@ -606,8 +563,6 @@ namespace test::token_stream
         const auto stream = ITokenInputStream::Create(ss);
         ss << static_cast<char>(TOKEN_SEMICOLON);
 
-        ss.seekg(0, std::ios::beg);
-
         REQUIRE_THROWS_AS(stream->NextStringValue(), TokenStreamException);
     }
 
@@ -618,8 +573,6 @@ namespace test::token_stream
         ss << static_cast<char>(TOKEN_FLOAT);
         constexpr float f = 4.20f;
         ss.write(reinterpret_cast<const char*>(&f), sizeof(f));
-
-        ss.seekg(0, std::ios::beg);
 
         const auto value = stream->NextFloatValue();
         REQUIRE_THAT(value, WithinAbs(static_cast<double>(4.20f), FLOAT_EPS));
@@ -634,8 +587,6 @@ namespace test::token_stream
         const auto stream = ITokenInputStream::Create(ss);
         ss << static_cast<char>(TOKEN_SEMICOLON);
 
-        ss.seekg(0, std::ios::beg);
-
         REQUIRE_THROWS_AS(stream->NextFloatValue(), TokenStreamException);
     }
 
@@ -644,8 +595,6 @@ namespace test::token_stream
         std::stringstream ss;
         const auto stream = ITokenInputStream::Create(ss);
         ss << static_cast<char>(TOKEN_SEMICOLON);
-
-        ss.seekg(0, std::ios::beg);
 
         REQUIRE_NOTHROW(stream->ExpectToken(TOKEN_SEMICOLON));
     }
@@ -656,8 +605,309 @@ namespace test::token_stream
         const auto stream = ITokenInputStream::Create(ss);
         ss << static_cast<char>(TOKEN_SEMICOLON);
 
-        ss.seekg(0, std::ios::beg);
-
         REQUIRE_THROWS_AS(stream->ExpectToken(TOKEN_COMMA), TokenStreamException);
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteString outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteString("Hello World");
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_STRING),
+            'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd',
+            '\x00'
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteFloat outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteFloat(1337.0f);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_FLOAT),
+
+            // 0x44a72000 -> 1337.0f
+            0x00, 0x20, 0xa7, 0x44
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteInteger outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteInteger(13361338);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_INT),
+            0xBA, 0xE0, 0xCB, 0x00
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure simple token methods output correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        token_type_t token = TOKEN_UNKNOWN;
+
+        SECTION("Token: left curly")
+        {
+            stream->WriteLeftCurly();
+            token = TOKEN_LEFT_CURLY;
+        }
+
+        SECTION("Token: right curly")
+        {
+            stream->WriteRightCurly();
+            token = TOKEN_RIGHT_CURLY;
+        }
+
+        SECTION("Token: left bracket")
+        {
+            stream->WriteLeftBracket();
+            token = TOKEN_LEFT_BRACKET;
+        }
+
+        SECTION("Token: right bracket")
+        {
+            stream->WriteRightBracket();
+            token = TOKEN_RIGHT_BRACKET;
+        }
+
+        SECTION("Token: comma")
+        {
+            stream->WriteComma();
+            token = TOKEN_COMMA;
+        }
+
+        SECTION("Token: semicolon")
+        {
+            stream->WriteSemicolon();
+            token = TOKEN_SEMICOLON;
+        }
+
+        const auto output = ss.str();
+
+        const uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(token)
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteInt8 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteInt8(42);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_INT8),
+            0x2A
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteUInt8 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteUInt8(174);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_UINT8),
+            0xAE
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteInt16 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteInt16(24750);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_INT16),
+            0xAE, 0x60
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteUInt16 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteUInt16(42134);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_UINT16),
+            0x96, 0xA4
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteFp16Man12 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteFp16Man12(3.03125f);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_FP16_MAN_12),
+            0x80, 0x30
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteFp16Man9 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteFp16Man9(42.96875f);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_FP16_MAN_9),
+            0x5F, 0x05
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteFp16Man0 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteFp16Man0(25931.0f);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_FP16_MAN_0),
+            0x4B, 0x65
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteFp8 outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteFp8(0.42f);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_FP8),
+            0x35
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteWChar outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteWChar(22077);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            static_cast<uint8_t>(TOKEN_WCHAR),
+            0x3D, 0x56
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
+    }
+
+    TEST_CASE("TokenOutputStream: Ensure WriteCustom outputs correct data", "[token][input]")
+    {
+        std::stringstream ss;
+        const auto stream = ITokenOutputStream::Create(ss);
+
+        stream->WriteCustom(42);
+
+        const auto output = ss.str();
+
+        constexpr uint8_t expectedOutput[]
+        {
+            0x2A
+        };
+
+        REQUIRE(output.size() == std::extent_v<decltype(expectedOutput)>);
+        REQUIRE(output == std::string(reinterpret_cast<const char*>(expectedOutput), std::extent_v<decltype(expectedOutput)>));
     }
 }
